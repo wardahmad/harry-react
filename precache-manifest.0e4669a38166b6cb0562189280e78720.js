5cf63e689180ab32c8662898b31983d424bbb51b
wardahmad <whitelight43@hotmail.com>
2020-01-17 22:24:27 +0300

    Updates

---
+++ precache-manifest.0e4669a38166b6cb0562189280e78720.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d34f5d813c679d78ac7cde5ce4feaae5",
+    "url": "/harry-react/index.html"
+  },
+  {
+    "revision": "27353c0da2e8324daad8",
+    "url": "/harry-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2ecc46b839efa2588ce7",
+    "url": "/harry-react/static/js/2.e88d5a7f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/harry-react/static/js/2.e88d5a7f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27353c0da2e8324daad8",
+    "url": "/harry-react/static/js/main.0596378f.chunk.js"
+  },
+  {
+    "revision": "9ab78fb19cce3da5e488",
+    "url": "/harry-react/static/js/runtime-main.9babd718.js"
+  }
+]);
